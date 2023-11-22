a774e34dfeacc5e2bce3b14963d22cb1517722ea
Wlfjd <leejiwon813@naver.com>
2023-11-22 21:07:32 +0900

    최신순, 평점순 정렬 버튼 sort 매소드로 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import { ReviewList } from "./components/ReviewList";
 import items from "./mock.json";
+import { useState } from "react";
 
 function App() {
-  return <ReviewList items={items}></ReviewList>;
+  //평점 높은 순으로 정렬하기
+  const [order, setOrder] = useState("");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]); // 동적인 속성 접근
+
+  return (
+    <>
+      <button onClick={() => setOrder("createdAt")}>최신순</button>
+      <button onClick={() => setOrder("rating")}>평점순</button>
+      <ReviewList items={sortedItems}></ReviewList>
+    </>
+  );
 }
 
 export default App;
