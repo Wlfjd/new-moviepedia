7233d9895b4f3f2c74b891036b11d8a8767c50b8
Wlfjd <leejiwon813@naver.com>
2023-11-23 20:52:43 +0900

    페이지네이션 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [items, setItems] = useState([]);
   //평점 높은 순으로 정렬하기
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
   const sortedItems = items.sort((a, b) => b[order] - a[order]); // 동적인 속성 접근
 
   const handleDelete = (id) => {
@@ -14,13 +15,18 @@ function App() {
     setItems(newItmes);
   };
 
-  const handleLoad = async (queryOrder) => {
-    const { reviews } = await getReviews(queryOrder);
-    setItems(reviews);
+  const handleLoad = async (option) => {
+    const { reviews } = await getReviews(option);
+    if (option.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(option.offset + reviews.length);
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset, limit: 6 });
   }, [order]);
 
   return (
@@ -28,6 +34,9 @@ function App() {
       <button onClick={() => setOrder("createdAt")}>최신순</button>
       <button onClick={() => setOrder("rating")}>평점순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete}></ReviewList>
+      <button onClick={() => handleLoad({ order, offset, limit: 6 })}>
+        더보기
+      </button>
     </>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
-export async function getReviews(order) {
+export async function getReviews({ order, offset, limit }) {
   // return 필수로 존재해야 한다 !! 없으면 데이터 못불러옴
-  return await fetch(`https://learn.codeit.kr/0627/film-reviews?order=${order}`)
+  return await fetch(
+    `https://learn.codeit.kr/0627/film-reviews?order=${order}&offset=${offset}&limit=${limit}`
+  )
     .then((res) => res.json())
     .then((data) => data);
 }
