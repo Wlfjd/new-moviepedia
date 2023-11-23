a532edf38a763d803accadf3c22901d06a21413c
Wlfjd <leejiwon813@naver.com>
2023-11-23 20:23:50 +0900

    서버에서 정렬된 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { getReviews } from "./api";
 import { ReviewList } from "./components/ReviewList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [items, setItems] = useState([]);
   //평점 높은 순으로 정렬하기
-  const [order, setOrder] = useState("");
+  const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]); // 동적인 속성 접근
 
   const handleDelete = (id) => {
@@ -14,16 +14,20 @@ function App() {
     setItems(newItmes);
   };
 
-  const handleClick = async () => {
-    const { reviews } = await getReviews();
+  const handleLoad = async (queryOrder) => {
+    const { reviews } = await getReviews(queryOrder);
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
+
   return (
     <>
       <button onClick={() => setOrder("createdAt")}>최신순</button>
       <button onClick={() => setOrder("rating")}>평점순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete}></ReviewList>
-      <button onClick={handleClick}>불러오기</button>
     </>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
-export async function getReviews() {
+export async function getReviews(order) {
   // return 필수로 존재해야 한다 !! 없으면 데이터 못불러옴
-  return await fetch("https://learn.codeit.kr/0627/film-reviews")
+  return await fetch(`https://learn.codeit.kr/0627/film-reviews?order=${order}`)
     .then((res) => res.json())
     .then((data) => data);
 }
