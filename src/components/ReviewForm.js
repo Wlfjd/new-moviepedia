22eef8e87ff1d60c48ecffb309257c311659ffee
Wlfjd <leejiwon813@naver.com>
2023-11-24 16:58:23 +0900

    하나의 state로 폼 구현(input의 name 속성 활용)

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,31 +2,38 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 export function ReviewForm() {
-  const [title, settitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({ title: "", rating: 0, content: "" });
+
+  const handleChange = (e) => {
+    setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    console.log(values);
+  };
+
   return (
     <form
       className="ReviewForm"
       onSubmit={(e) => {
-        console.log(title, rating, content);
+        console.log(values);
         e.preventDefault();
       }}
     >
       <input
-        value={title}
+        value={values.title}
+        name="title"
         placeholder="영화제목을 입력해주세요"
-        onChange={(e) => settitle(e.target.value)}
+        onChange={handleChange}
       ></input>
       <input
-        value={rating}
+        value={values.rating}
+        name="rating"
         type="number"
-        onChange={(e) => setRating(Number(e.target.value) || 0)}
+        onChange={handleChange}
       ></input>
       <textarea
-        value={content}
+        value={values.content}
+        name="content"
         placeholder="영화내용을 입력해주세요"
-        onChange={(e) => setContent(e.target.value)}
+        onChange={handleChange}
       ></textarea>
       <button>확인</button>
       {/* 일반적으로 폼 내에서 <button>을 사용하면 이 버튼은 "submit" 유형의 버튼으로 간주되어 폼을 제출 */}
